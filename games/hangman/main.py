e4bcedf34cc7b67eef7e285c9dd9ed812fe73e63
B. Matthews <boikie.sealetsa@standardbank.co.za>
2021-09-19 09:00:52 +0200

    Update functions name and remove unnecessary functions

--- games/hangman/main.py
+++ games/hangman/main.py
@@ -5,12 +5,12 @@ import random
 def printLogo():
     print(logo)
 
-def isLetterGuessed(guess, guessed_letters):
+def check_repeated_letter(guess, guessed_letters):
     """Checks if the guessed letter was already guessed before."""
     if guess in guessed_letters:
             print(f"You already guessed {guess}")
 
-def checkGuessedLetter(chosen_word, guess, display):
+def verify_letter_guess(chosen_word, guess, display):
     """Checks if the guessed letter is part of the word and returns the updated user's displayed guessed word."""
     for position in range(len(chosen_word)):
         letter = chosen_word[position]
@@ -19,32 +19,19 @@ def checkGuessedLetter(chosen_word, guess, display):
     
     return display
 
-def isGuessWrong(guess, chosen_word, guessed_letters, lives, end_of_game):
-    """Checks if the guessed letter is part of the word, returns the the updated number of lives and the status of the game."""
+def update_game_status(guess, chosen_word, guessed_letters, lives, end_of_game, display):
+    """Checks the status of the game, returns the the updated number of lives and the upadated status of the game."""
     if guess not in chosen_word and guess not in guessed_letters:
         print(f"you guessed {guess}, that is not in the word. You lose a life.")
         lives -= 1
         if lives == 0:
             end_of_game = True
             print("You lose.")
-                
-    return end_of_game, lives
-
-def allLettersGuessed(display, end_of_game):
-    """Checks if all the letters are guessed and returns the status of the game."""
-    if "_" not in display:
+    elif "_" not in display:
         end_of_game = True
         print("You win.")
-    
-    return end_of_game
-
-def printStage(lives):
-    """Prints each stage of the game."""
-    print(stages[lives])
-
-def printWord(chosen_word):
-    """"Prints the word being guessed."""
-    print(f"The word is {chosen_word}")
+                
+    return end_of_game, lives
 
 def startGame():
     chosen_word = random.choice(word_list)
@@ -64,15 +51,14 @@ def startGame():
         while len(guess) is not 1 or not guess.isalpha():
             guess = input("Guess a single letter [A-z]: ").lower()
 
-        isLetterGuessed(guess, guessed_letters)        
-        display = checkGuessedLetter(chosen_word, guess, display)
-        end_of_game, lives = isGuessWrong(guess, chosen_word, guessed_letters, lives, end_of_game)
+        check_repeated_letter(guess, guessed_letters)        
+        display = verify_letter_guess(chosen_word, guess, display)
+        end_of_game, lives = update_game_status(guess, chosen_word, guessed_letters, lives, end_of_game, display)
         print(f"{' '.join(display)}")
-        end_of_game = allLettersGuessed(display, end_of_game)
-        printStage(lives)
+        print(stages[lives])
         guessed_letters.append(guess)
 
-    printWord(chosen_word)
+    print(f"The word is {chosen_word}")
 
 if __name__ == '__main__':
     startGame()
